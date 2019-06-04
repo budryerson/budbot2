f6dfead19221bd52dbd42cc7a10292cd5463a2d9
Bud Ryerson <bud@budryerson.com>
2019-06-03 18:08:04 -0700

    Joystick syncs with platform
    and other imporvements

--- bb2_joy/bb2_joy.ino
+++ bb2_joy/bb2_joy.ino
@@ -1,6 +1,5 @@
-
 /* File Name: bb2_joy.ino
- * Described: THIS IS THE TRANSMITTER
+ * Described: THIS IS THE SLAVE/RF RECEIVER
  * Developer: Bud Ryerson
  * Inception: 19APR2015
  * 03AUG15 - reworked the button debounce code
@@ -17,125 +16,143 @@
  * 11FEB17 - Severely rearranged commands in attempt
              to synchronize joystick with platform
    20JUL17 - Both running 50ms loops but NOT synchronized
+   18NOV17 - Got rid of loop timer and replace with radio IRQ
+             to synchronize the controller with the platform.
+             The Loop runs free and services the radio upon request.
+   15DEC17 - Modified joytick.cpp to send HEX numbers in effort
+             to shorten serial transmission time.
+   08MAR19 - Working in the coal mine.. going down, down, down.
+             Trying to rehabilitate budbot2.
+             Getting radio IRQs. Yippee!
  */
 
 #include <Arduino.h>
-#include <nRF24L01.h>
+#include <SPI.h>          //  nRF24 Radio SPI Communication
+#include <printf.h>       //  printf is used everywhere
+#include <nRF24L01.h>     //  nRF24 Radio Library
 #include <RF24.h>
 #include <RF24_config.h>
-#include <SPI.h>
 
+/* - - - - - - - - budbot libraries - - - - - - - */
 #include <bb_defines.h>
-#include <bb_shared.h>
-#include <bb_routines.h>
-#include <printf.h>
+#include <bb_shared.h>    // global variables
+#include <bb_routines.h>  // global functions
 
-//  include BudBot libraries
-#include <bb_joystick.h>
-bb_joystick bbJoystick;
-#include <bb_radio.h>
-bb_radio bbRadio;
+#include <bb_joystick.h>   // load the joystick library
+bb_joystick bbJoystick;    // initialize a jostick object
+#include <bb_radio.h>      // load the radio library
+bb_radio bbRadio;          // initalize a radio object
+/* - - - - - - - - - - - - - - - - - - - - - - - - - */
 
-//  25ms Timer Counter 2A Interrupt Service Routine
-//  TCNT2 is set to 12.5ms in 'routines.cpp'
-bool t2Flag = false;
-int t2Count = 0;
-ISR( TIMER2_COMPA_vect)   //  Timer 2A ISR triggers every 12.5ms
+//  = = = Joystick Radio Interrupt Service Routines (ISR) = = =
+//  Set flag TRUE when radio service is requested
+volatile bool radioFlag;
+void jriSR()       // attached to Interrupt 0 below
 {
-    t2Flag = true;
-    ++t2Count;
+   radioFlag = true;
 }
+/* - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 void setup()
 {
     Serial.begin( 115200);   //  initialize the serial port
+    Serial.flush();          //  flush the serial buffer
     printf_begin();          //  initialize the printf module
     SPI.begin();             //  initiate the SPI object for the Radio
 
     bbRadio.setupJoystickRadio();
     bbJoystick.setup();       //  initialize ProMini pins
-    setupTimer2();            //  Setup Timer 3 is also in 'routines.cpp'    
     clearDataStructures();    //  clear all platform and joystick data structures
-                              //  set boolean values to false
-                              //  in 'routines.cpp'
+                              //  and set boolean values to false - 'routines.cpp'
+    jDat.mpxID = JDAT_ID;     //  set to 0x00CD or 0b11001101 - 'defines.h'
+
+    attachInterrupt( 0, jriSR, FALLING);   // Attach joystick radio ISR
+                                           // to Interrupt 0 (pin 2) going low.
+    radioFlag = false;                     // wait for Radio INT 0 set set true
 }
 
 void loop()
 {
-/* 
-    bbRadio.joystickRadioAck();
-    if( jDat.radio)
-    {
-      jDat.loopTime = (uint8_t)( millis() - jDat.joyClock);
-      jDat.joyClock = millis();  // get the time
-      ++jDat.joyLoop;         // increase the joystick loop counter
+      jDat.joyClock = millis(); // save system clock in milliseconds
+      bbJoystick.readData();    // Get joystick position data, and test
+                                // buttons to set jDat signal bits.
 
-      bbJoystick.readData();   // Get joystick position data, check
-                               // buttons and set jDat signal bits.
-      bbJoystick.checkJoyButtons();
-      printTimeData();
+      //  If two rDat packets have been received and their loTime bytes agree
+      //  (jDat.sync),then do 'putPaneData' and 'getPaneData' routines to and
+      //  from the PC display Processing package
+      if( jDat.sync)
+      {
+          bbJoystick.putPaneData();    //  Send platform (rData) to the PC;
+          jDat.sigBits = 0;            //  Clear joystick signal bits
+          bbJoystick.getPaneData();    //  then get serial signal data from PC
+          digitalWrite( jBluPin, !digitalRead(jBluPin));   //  Toggle blue LED
+  
+          jDat.sync = false;    // reset the sync flag
+          jDat.radio1 = false;
+          jDat.radio2 = false;
+      }
 
-      //bbJoystick.putPaneData();    //  Send platform (rData) to the PC;
-      //bbJoystick.getPaneData();    //  then get serial signal data from PC
-      //  printAllData();              //  data struct in Hexs - 'routines.cpp'
+      // If a radio IRQ occurs, then:
+      // 1) Call the 'joystickRadioAck()' function to:
+      //   a) read a platform data packets (rDat1) and send a
+      //   joystick data packet (jDat) as acknowledgment (ACK);
+      //   b) wait for a second platform data packet (rDat2)
+      //   and acknowledg that;
+      // 2) show the lights;
+      // 3) reset the radio IRQ flag and
+      // 4) return to Main Loop.
+      if( radioFlag)
+      {
+          // I don't know why I changed from 'radioFlag' to
+          // 'jDat.radioIRQ' so I'm toggling it and counting
+          //  them up to 1000 for the hell of it.
+          jDat.radioIRQ = !jDat.radioIRQ;
+          if( jDat.rIrqCount < 1000) ++jDat.rIrqCount;
+              else jDat.rIrqCount = 0;
 
-      bbJoystick.showLights(); // Light LEDS to match rDat data flag bits
-    }
-}
-*/
+          bbRadio.joystickRadioAck();  // Run the radio routine.
+          bbJoystick.showLights();     // Light LEDS to match rDat flag bits.
+          jDat.radioIRQ = false;
+          radioFlag = false;           // Reset the radio IRQ flag.
+      }
 
-  //bbJoystick.checkJoyButtons();
-  //  Every 12.5ms, the Timer2 Interrupt Service Routine sets tc2Flag TRUE,
-  //  and advances the t2 counter.  The last statement of the loop resets
-  //  the T2 flag to FALSE.
-  if( t2Flag)
-  {
-    //  COM loop runs at start of every 3 count sequence.
-    //  (3 * 12.5ms = 37.5ms)
-    if( t2Count < 2)   // counter equals 1, run communication routine
-    {
-      jDat.loopTime = (uint8_t)( millis() - jDat.joyClock);
-      jDat.joyClock = millis();  // get system time in milliseconds
-      ++jDat.joyLoop;            // advance joystick loop counter
-
-      bbJoystick.readData();   // Get joystick position data, check
-                               // buttons and set jDat signal bits.
-      bbJoystick.checkJoyButtons();
-      //  printTimeData();
-
-      bbJoystick.putPaneData();    //  Send platform (rData) to the PC;
-      bbJoystick.getPaneData();    //  then get serial signal data from PC
-      // printAllData();              //  data struct in Hexs - 'routines.cpp'
-
-      //  Radio write joystick data packet (jDat) twice and
-      //  read two platform packets (rDat) as acknowledgments.
-      bbRadio.joystickRadioAck();
-      bbJoystick.showLights(); // Light LEDS to match rDat data flag bits      
-    }
-    else if( t2Count < 3);    // counter equals 2, do nothing
-    else if( t2Count < 4);    // counter equals 3, do nothing
-    else t2Count = 0;         // counter equals 4 or more, reset
-    t2Flag = false;           // reset timer 2 flag
-  }
+//    printAllData();    // print all data structs in Hex code - 'routines.cpp'
+      ++jDat.joyLoop;      // advance joystick loop counter
+      jDat.loopTime = (uint16_t)( millis() - jDat.joyClock);
+//    printTimeData();
+//    bbJoystick.printJoyData();
+//    delay(20);
+//    digitalWrite( jBluPin, !digitalRead(jBluPin));   //  Toggle blue LED
+//    bbJoystick.showLights();     // Light LEDS to match rDat flag bits.
 }
 
+//  This is for testing purposes
 void printTimeData()
 {
+      printf( "jLoop %07u", jDat.joyLoop);
+      printf( " | ");
       printf( "jClk %010u", jDat.joyClock);  //   Joystick Clock in milliseconds
       printf( " | ");
       printf( "jt %03u", jDat.loopTime);  //   Joystick loop time
       printf( " | ");
       printf( "jRad: ");
-      if( jDat.radio) printf( "True "); else printf( "False");
+      if( jDat.radioIRQ) printf( "True "); else printf( "False");
+      printf( " | ");
+      printf( "jRad1: ");
+      if( jDat.radio1) printf( "True "); else printf( "False");
+      printf( " | ");
+      printf( "jRad2: ");
+      if( jDat.radio2) printf( "True "); else printf( "False");
+      printf( " | ");
+      printf( "jSync: ");
+      if( jDat.sync) printf( "True "); else printf( "False");
       printf( " | ");
       printf( "r1 %03u", rDat1.loTime);   //   Short Platform Clock in milliseconds
       printf( " | ");
       printf( "r2 %03u", rDat2.loTime);     //   Short Platform Clock in milliseconds
       printf( " | ");
+      printf( "bRadio %03u", rDat2.radioTime);  //   Platform loop time
+      printf( " | ");
       printf( "bLoop %04u", rDat2.loopTime);  //   Platform loop time
-      printf( " | ");      
-      printf( "jSync: ");
-      if( jDat.sync) printf( "True "); else printf( "False");
       printf( "\r\n"); // carriage return/line feed
-      
 }
